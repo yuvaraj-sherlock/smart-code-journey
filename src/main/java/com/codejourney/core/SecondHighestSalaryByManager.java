c6158b2cae2f2bcd947aeb4dd85424061c245ffe
Yuvaraj <yuvaraj.sf.java@gmail.com>
2026-04-16 19:36:18 +0530

    Find SecondHighestSalariedEmployeeByManager program

---
+++ src/main/java/com/codejourney/core/SecondHighestSalaryByManager.java
@@ -0,0 +1,40 @@
+package com.codejourney.core;
+
+import com.codejourney.dto.Employee;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/*
+Problem: Given a list of employees, find the second-highest salaried employee under each manager.
+Approach: Group employees by manager and identify the top 2 salaries within each group.
+
+Pattern: Map + Grouping + Top-K
+Trigger: "for each manager" + "second highest"
+Core Tweak: Maintain top 2 salaries
+*/
+public class SecondHighestSalaryByManager {
+  public static void main(String[] args) {
+    List<Employee> employeeSampleData = Employee.getEmployeeSampleData();
+
+    Map<String, List<Employee>> managerMap = employeeSampleData.stream()
+            .collect(Collectors.groupingBy(Employee::manager));
+
+    managerMap.forEach((manager, employeeList)->{
+          System.out.println("Manager: "+manager);
+
+          if(employeeList.size()<2){
+            System.out.println("Not enough employees");
+            return;
+          }
+
+          employeeList.stream()
+                  .sorted(Comparator.comparingDouble(Employee :: salary).reversed())
+                  .skip(1)
+                  .findFirst()
+                  .ifPresent(System.out::println);
+    });
+  }
+}
---
+++ src/main/java/com/codejourney/dto/Employee.java
@@ -0,0 +1,23 @@
+package com.codejourney.dto;
+
+import java.util.List;
+
+public record Employee(int id,
+                       String name,
+                       double salary,
+                       String manager,
+                       String city,
+                       String dept) {
+
+  public static List<Employee> getEmployeeSampleData() {
+    return List.of(
+            new Employee(1, "Alice", 90000, "M1", "Bangalore", "IT"),
+            new Employee(2, "Bob", 75000, "M1", "Hyderabad", "IT"),
+            new Employee(3, "Charlie", 80000, "M1", "Chennai", "IT"),
+
+            new Employee(4, "David", 95000, "M2", "Pune", "HR"),
+            new Employee(5, "Eve", 70000, "M2", "Mumbai", "HR"),
+            new Employee(6, "Frank", 85000, "M2", "Delhi", "HR")
+    );
+  }
+}
