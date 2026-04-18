045d2fd1b6ee74cfcf10000442cfe5dc7126eeb5
Yuvaraj <yuvaraj.sf.java@gmail.com>
2026-04-18 09:20:52 +0530

    few more utility methods

--- src/main/java/com/codejourney/core/CustomPattern.java
+++ src/main/java/com/codejourney/core/CustomPattern.java
@@ -1,12 +1,15 @@
 package com.codejourney.core;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /*Custom pattern finding and fix the problem*/
 
 /*
 Important methods:
       Arrays.toString(array); //To print array
+
 */
 
 public class CustomPattern {
@@ -14,6 +17,9 @@ public class CustomPattern {
 
     int[] input = {36,11,6,9,45,4};
     arraySortAndSwapPattern(input);
+
+    int[] input2 = {0,1,0,3,12};
+    moveZerosToEndOfArray(input2);
   }
 
   /*Input: 36,11,6,9,45,4
@@ -30,5 +36,35 @@ public class CustomPattern {
       }
     }
     System.out.println(Arrays.toString(input));
+  }
+
+  //  Input: [0,1,0,3,12] → Output: [1,3,12,0,0]
+  private static void moveZerosToEndOfArray(int[] input2) {
+    //1st approach:
+    List<Integer> list = new ArrayList<>();
+    int zeroCount = 0;
+    for (int i : input2) {
+      if (i == 0) {
+        zeroCount++;
+      } else {
+        list.add(i);
+      }
+    }
+    for (int i = 0; i < zeroCount; i++) {
+      list.add(0);
+    }
+    System.out.println(list);
+
+    //2nd approach:
+    int currentPos = 0;
+    for (int i : input2) {
+      if (i != 0) {
+        input2[currentPos++] = i;
+      }
+    }
+    for (int i = currentPos; i < input2.length; i++) {
+      input2[currentPos++] = 0;
+    }
+    Arrays.stream(input2).forEach(num -> System.out.print(num + ","));
   }
 }
